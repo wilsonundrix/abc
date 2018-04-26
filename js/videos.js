2241644ef92461c4664dbb576277ed69cbfa8fcc
ndunya <ndunyaundrix@gmail.com>
2018-04-26 20:29:59 +0300

    commiting crazy updates

---
+++ js/videos.js
@@ -0,0 +1,6 @@
+function setAsMainVideo(ind){
+	document.getElementById('plvideo').src = document.getElementById(ind).src;
+	// var vid = document.getElementById(ind);
+	// var path = vid.src;
+	// document.getElementById('crae').innerHTML = path ;
+}
